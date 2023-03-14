11b6d4f29062de4f34560b5f9e9ef07c0cf50e3c
Guilherme Gomes <guilhermegg018@gmail.com>
2023-03-14 01:19:35 -0300

    página do carrinho de compras e página de detalhes feitas

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,16 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/cart_item.dart';
+import 'package:shop/models/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
+    final items = cart.items.values.toList();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Carrinho'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (ctx, idx) => Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: CartItemWidget(
+                  items[idx],
+                ),
+              ),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.symmetric(vertical: 25, horizontal: 15),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Total:',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Chip(
+                    backgroundColor: Colors.purple,
+                    label: Text(
+                      'R\$${cart.totalAmount}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        textStyle: const TextStyle(
+                      color: Colors.purple,
+                    )),
+                    onPressed: () {},
+                    child: const Text('COMPRAR'),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/pages/produtct_detail_page.dart
+++ lib/pages/produtct_detail_page.dart
@@ -8,13 +8,44 @@ class ProdutctDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Product product = ModalRoute.of(context)!.settings.arguments as Product;
+    final Product product =
+        ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         title: Text(product.name),
         centerTitle: true,
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'R\$ ${product.price}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
